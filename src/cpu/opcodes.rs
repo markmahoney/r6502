ca6f36d40611c42d8010d81ccac111ab92faae07
Mark Mahoney <bcmark@gmail.com>
2022-07-30 21:56:16 -0700

    sick, we can now run a program that adds two small numbers together

--- src/cpu/opcodes.rs
+++ src/cpu/opcodes.rs
@@ -13,6 +13,68 @@ pub trait Instruction {
     fn cycle(&mut self, step: usize, reg: &mut Registers, address_bus: &mut dyn IODevice) -> InstructionState;
 }
 
+// ADC
+// absolute
+// TODO: handle decimal mode
+struct ADC0x6D {
+    adh: u8,
+    adl: u8,
+}
+impl ADC0x6D {
+    fn new() -> Option<Box<dyn Instruction>> { Some(Box::new(Self { adh: 0, adl: 0 })) }
+}
+impl Instruction for ADC0x6D {
+    fn cycle(&mut self, cycle: usize, reg: &mut Registers, address_bus: &mut dyn IODevice) -> InstructionState {
+        match cycle {
+            0 => {
+                self.adl = reg.data;
+                InstructionState::Continue
+            },
+            1 => {
+                self.adh = reg.data;
+                InstructionState::Continue
+            },
+            2 => {
+                (reg.adh, reg.adl) = (self.adh, self.adl);
+                reg.data = address_bus.get_hl(reg.adh, reg.adl);
+
+                let total: u16 = reg.a as u16 + reg.data as u16 + if reg.status.flag(StatusRegister::CARRY) { 1 } else { 0 };
+                reg.a = total as u8;
+
+                if total > 255 {
+                    reg.status.set_flag(StatusRegister::CARRY);
+                } else {
+                    reg.status.clear_flag(StatusRegister::CARRY);
+                }
+
+                if reg.a == 0 {
+                    reg.status.set_flag(StatusRegister::ZERO);
+                } else {
+                    reg.status.clear_flag(StatusRegister::ZERO);
+                }
+
+                if reg.a & 0b10000000 == 0b10000000 {
+                    reg.status.set_flag(StatusRegister::NEGATIVE);
+                } else {
+                    reg.status.clear_flag(StatusRegister::NEGATIVE);
+                }
+
+                if (reg.a & 0b10000000) != ((total as u8) & 0b10000000) {
+                    reg.status.set_flag(StatusRegister::OVERFLOW);
+                } else {
+                    reg.status.clear_flag(StatusRegister::OVERFLOW);
+                }
+
+                reg.pc -= 1;
+                InstructionState::Continue
+            },
+            3 => InstructionState::Finished,
+            _ => panic!("unexpected cycle for ADC0x6D: {cycle}")
+        }
+    }
+}
+
+
 // CLC
 // implied
 struct CLC0x18;
@@ -79,14 +141,61 @@ impl Instruction for LDA0xA9 {
                 } else {
                     reg.status.clear_flag(StatusRegister::ZERO);
                 }
+
                 if reg.a & 0b10000000 == 0b10000000 {
                     reg.status.set_flag(StatusRegister::NEGATIVE);
                 } else {
                     reg.status.clear_flag(StatusRegister::NEGATIVE);
                 }
+
                 InstructionState::Finished
             },
             _ => panic!("unexpected cycle for LDA0xA9: {cycle}")
+        }
+    }
+}
+
+// absolute
+struct LDA0xAD {
+    adh: u8,
+    adl: u8,
+}
+impl LDA0xAD {
+    fn new() -> Option<Box<dyn Instruction>> { Some(Box::new(Self { adh: 0, adl: 0 })) }
+}
+impl Instruction for LDA0xAD {
+    fn cycle(&mut self, cycle: usize, reg: &mut Registers, address_bus: &mut dyn IODevice) -> InstructionState {
+        match cycle {
+            0 => {
+                self.adl = reg.data;
+                InstructionState::Continue
+            },
+            1 => {
+                self.adh = reg.data;
+                InstructionState::Continue
+            },
+            2 => {
+                (reg.adh, reg.adl) = (self.adh, self.adl);
+                reg.data = address_bus.get_hl(reg.adh, reg.adl);
+                reg.a = reg.data;
+
+                if reg.a == 0 {
+                    reg.status.set_flag(StatusRegister::ZERO);
+                } else {
+                    reg.status.clear_flag(StatusRegister::ZERO);
+                }
+
+                if reg.a & 0b10000000 == 0b10000000 {
+                    reg.status.set_flag(StatusRegister::NEGATIVE);
+                } else {
+                    reg.status.clear_flag(StatusRegister::NEGATIVE);
+                }
+
+                reg.pc -= 1;
+                InstructionState::Continue
+            },
+            3 => InstructionState::Finished,
+            _ => panic!("unexpected cycle for LDA0xAD: {cycle}")
         }
     }
 }
@@ -114,12 +223,12 @@ impl Instruction for STA0x8D {
             2 => {
                 (reg.adh, reg.adl) = (self.adh, self.adl);
                 reg.data = reg.a;
-                address_bus.put_hl(reg.adh, reg.adl, reg.a);
+                address_bus.put_hl(reg.adh, reg.adl, reg.data);
                 reg.pc -= 1;
                 InstructionState::Continue
             },
             3 => InstructionState::Finished,
-            _ => panic!("unexpected cycle for LDA0xA9: {cycle}")
+            _ => panic!("unexpected cycle for STA0x8D: {cycle}")
         }
     }
 }
@@ -127,8 +236,11 @@ impl Instruction for STA0x8D {
 pub fn find_instruction(opcode: u8) -> Option<Box<dyn Instruction>> {
     match opcode {
         0x18 => CLC0x18::new(),
+        0x60 => None, // RTS, not sure how to implment this yet, so just exit when we hit it
+        0x6d => ADC0x6D::new(),
         0x8d => STA0x8D::new(),
         0xa9 => LDA0xA9::new(),
+        0xad => LDA0xAD::new(),
         0xd8 => CLD0xD8::new(),
         _ => None
     }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ pub mod memory;
 use crate::address_decoder::AddressDecoder;
 use crate::clock::Clock;
 use crate::cpu::cpu_6502::CPU6502;
-use crate::cpu::status_register::StatusRegister;
 use crate::io_device::IODevice;
 use crate::memory::ram::RAM;
 use crate::memory::rom::ROM;
@@ -16,18 +15,18 @@ fn print_state(cpu: &CPU6502, address_line: &AddressDecoder) {
     println!("Status register: {:#010b}", cpu.registers.status.flags);
     println!("Accumulator: {:#04x}", cpu.registers.a);
     println!("Data bus: {:#04x}", cpu.registers.data);
-    println!("ADH and ADL: {:#04x} {:#04x}", cpu.registers.adh, cpu.registers.adl);
+    println!("ADH/ADL: {:#04x} {:#04x}", cpu.registers.adh, cpu.registers.adl);
     println!("Contents of 0x6100 to 0x6102: {:#04x} {:#04x} {:#04x}",
              address_line.get(0x6100),
              address_line.get(0x6101),
-             address_line.get(0x6103)
+             address_line.get(0x6102)
     );
 }
 
 fn main() {
-    let clock = Clock::new(1.0);
-    let mut cpu = CPU6502::new();
+    let clock = Clock::new(1000000.0);
     let mut address_line = AddressDecoder::new();
+    let mut cpu = CPU6502::new();
 
     address_line.add_device(
         0x0000..=0x7FFF,
@@ -41,16 +40,11 @@ fn main() {
         Box::new(ROM::<0x8000>::new(Some([255; 0x8000]))),
     );
 
-    cpu.registers.status.set_flag(StatusRegister::CARRY);
-    cpu.registers.status.set_flag(StatusRegister::DECIMAL_MODE);
-
     // Go until we reach an opcode we don't know how to run
     clock.start(|| {
+        println!("\n=== Clock Cycle ===");
         let cont = cpu.tick(&mut address_line);
-
         print_state(&cpu, &address_line);
-        println!("===");
-
         cont
     });
 }
